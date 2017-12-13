cd41699417b22a2e83b27392e856b8f8cbf4b612
adighate10 <adighate10@gmail.com>
2017-12-13 11:50:27 +0530

    changed hello message

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -12,7 +12,7 @@ class MainController extends Controller
         $data = $request->all();
         //get the user’s id
         $id = $data["entry"][0]["messaging"][0]["sender"]["id"];
-        $this->sendTextMessage($id, "Hello");
+        $this->sendTextMessage($id, "Welcome to Gleamlight");
     }
 
     private function sendTextMessage($recipientId, $messageText)
